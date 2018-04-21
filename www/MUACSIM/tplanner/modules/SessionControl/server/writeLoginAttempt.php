df5d7ff35571e2d00a06ab09824632c251b93b86
MOLTENI Alberto <alberto.molteni@eurocontrol.int>
2018-04-21 11:50:39 +0200

    debug SessionControl

--- www/MUACSIM/tplanner/modules/SessionControl/server/writeLoginAttempt.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-file_put_contents("../../../loginAttempt.txt","$_SERVER[REMOTE_ADDR],$_POST[username],$_POST[passwd],0");
--- www/MUACSIM/tplanner/modules/SessionControl/server/writeLoginResult.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-file_put_contents("../../../loginAttempt.txt",substr(file_get_contents("../../../loginAttempt.txt"),0,-1) . $_GET['result']);
