517ef8269fdd3352c956fd8249867b6a605dbfc3
MOLTENI Alberto <alberto.molteni@eurocontrol.int>
2018-11-26 10:27:59 +0100

    debug

--- www/MUACSIM/tplanner/modules/MyDuties/js/MiniPlanner.js
+++ www/MUACSIM/tplanner/modules/MyDuties/js/MiniPlanner.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 			events              : ev2,
 			selectable          : true,
 			select              : false,
-			viewRender          : function( ){setTimeout(function(){$(".needs-mfs .fc-content").append('<span class="badge badge-default" style="position:absolute;right:0;padding:1px 2px 2px 2px;">MFS</span>')},100)},
+			viewRender          : function(){setTimeout(function(){knownHolidays_dates.map(function(khd){$(".fc-bg .fc-day[data-date="+khd+"]").addClass("fc-sun")});$(".fc-scroller").css("height","55vh")},200)},
 			timeFormat          : 'HH:mm',
 			columnFormat        : 'ddd DD-MM',
 			defaultView         : 'basView',
@@ -53,5 +53,8 @@ $(document).ready(function(){
 									}
 			}
 		});
+		$(".fc-toolbar h2").css("font-size","1.4rem").css("margin-bottom","0.8rem");
+		$(".fc-toolbar button").css("font-size","0.9em");
+		$("head").append("<style>.fc-month-view .fc-time{display:none}.fc-day-grid-event{text-align:center}.fc-agenda-view .fc-day-header span{font-size:0.8em}</style>");
 	});
 });
