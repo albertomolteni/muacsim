f7abb145ad09910d5dab9baa438c0a8fc300f58e
MOLTENI Alberto <alberto.molteni@eurocontrol.int>
2018-11-26 09:58:56 +0100

    debug

--- www/MUACSIM/tplanner/modules/MyDuties/js/MiniPlanner.js
+++ www/MUACSIM/tplanner/modules/MyDuties/js/MiniPlanner.js
@@ -1,9 +1,19 @@
+function requiredRolesHTML(s)
+{
+	if (s == '') return '';
+	var a = ['','','','Pilots','Hybrids DECO','Hybrids BRU','Hybrids HANN','OJTIs DECO','OJTIs BRU','OJTIs HANN'];
+	var o = [];
+	var r = s.split(',');
+	r.map(function(x){if(x.split(':')[0]>2)o.push((x.split(':')[1]+' '+a[x.split(':')[0]/1]).replace(/1 (.+)s/,'1 $1'))});
+	return '<br>Requires ' + o.join(', ');
+}
+
 $(document).ready(function(){
 	knownHolidays_dates  = ['2019-01-01','2019-01-02','2019-04-19','2019-04-22','2019-05-30','2019-05-31','2019-06-10','2019-12-24','2019-12-25','2019-12-26','2019-12-27'];
 	knownHolidays_titles = ['New Year','New Year','Good Friday','Easter Monday','Ascension','Ascension','Whit Monday','Christmas Eve','Christmas','Boxing Day','Year-end Closure'];
 	
 	$.vPOST("/MUACSIM/tplanner/modules/Planner/server/readSimEvents.php",null,function(resp){
-		ev1      = $.parseJSON(resp);
+		ev1 = $.parseJSON(resp);
 		for (var evi=ev1.length-1;evi>=0;evi--) if (ev1[evi].sim>9) ev1.splice(evi,1);
 		ev1.map(function(a){
 			a.id        = a.simeventID;
@@ -22,7 +32,7 @@ $(document).ready(function(){
 			events              : ev2,
 			selectable          : true,
 			select              : false,
-			viewRender          : function( ){setTimeout(function(){$("[data-toggle=tooltip]").tooltip();$(".needs-mfs .fc-content").append('<span class="badge badge-default" style="position:absolute;right:0;padding:1px 2px 2px 2px;">MFS</span>')},100)},
+			viewRender          : function( ){setTimeout(function(){$(".needs-mfs .fc-content").append('<span class="badge badge-default" style="position:absolute;right:0;padding:1px 2px 2px 2px;">MFS</span>')},100)},
 			timeFormat          : 'HH:mm',
 			columnFormat        : 'ddd DD-MM',
 			defaultView         : 'basView',
