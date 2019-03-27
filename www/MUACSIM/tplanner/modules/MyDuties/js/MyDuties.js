d1714e3a76adad6958649676a7fc63effd33a2d4
albertomolteni <alberto_molteni@yahoo.com>
2019-03-27 08:50:21 +0100

    Update MyDuties.js

--- www/MUACSIM/tplanner/modules/MyDuties/js/MyDuties.js
+++ www/MUACSIM/tplanner/modules/MyDuties/js/MyDuties.js
@@ -199,8 +199,8 @@ $(document).ready(function(){
 	d.setHours(4);
 	d.setDate(d.getDate() - 1);
 	
-	knownHolidays_dates  = ['2019-01-01','2019-01-02','2019-04-19','2019-04-22','2019-05-30','2019-05-31','2019-06-10','2019-12-24','2019-12-25','2019-12-26','2019-12-27'];
-	knownHolidays_titles = ['New Year','New Year','Good Friday','Easter Monday','Ascension','Ascension','Whit Monday','Christmas Eve','Christmas','Boxing Day','Year-end Closure'];
+	knownHolidays_dates  = ['2019-01-01','2019-01-02','2019-04-19','2019-04-22','2019-05-30','2019-05-31','2019-06-10','2019-12-24','2019-12-25','2019-12-26','2019-12-27','2020-01-01','2020-01-02','2020-04-10','2020-04-13','2020-05-21','2020-05-22','2020-06-01','2020-12-24','2020-12-25'];
+	knownHolidays_titles = ['New Year','New Year','Good Friday','Easter Monday','Ascension','Ascension','Whit Monday','Christmas Eve','Christmas','Boxing Day','Year-end Closure','New Year','New Year','Good Friday','Easter Monday','Ascension','Ascension','Whit Monday','Christmas Eve','Christmas'];
 	
 	var template = $("#dutyCalendarTemplate").html();
 	
